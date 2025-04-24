192f1f2753654733e73742851e359e70cb6055fa
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-04-24 15:57:56 -0300

    first commit

---
+++ operacoes.go
@@ -0,0 +1,18 @@
+package main
+
+func soma(x, y int) int {
+	return x + y
+}
+
+func subtrair (x, y int) int {
+	return x - y
+}
+
+func multiplicao (x, y int) int{
+	return x * y
+}
+
+func divisao (x, y int) int{
+	return x / y
+}
+
